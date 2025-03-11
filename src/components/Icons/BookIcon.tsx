6560d5d164cb8dcbfec833b2213c2728ad46d779
trungh97 <hoangquoctrungit@gmail.com>
2025-03-11 21:06:13 +0700

    Add some icons

---
+++ src/components/Icons/BookIcon.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+
+const BookIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#181D27"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20M4 19.5A2.5 2.5 0 0 0 6.5 22H20V2H6.5A2.5 2.5 0 0 0 4 4.5v15Z"
+    />
+  </svg>
+);
+export default BookIcon;
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -3,4 +3,6 @@ export { default as VectorIcon } from './VectorIcon';
 export { default as MailIcon } from './MailIcon';
 export { default as ErrorCircleIcon } from './ErrorCircleIcon';
 export { default as UserIcon } from './UserIcon';
+export { default as MessageIcon } from './MessageIcon';
+export { default as BookIcon } from './BookIcon';
 export * from './SocialMedia';

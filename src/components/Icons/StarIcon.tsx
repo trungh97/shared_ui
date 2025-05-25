bb296381a0bae24c18332a94bbfa5ab19f3289c4
trungh97 <hoangquoctrungit@gmail.com>
2025-05-25 15:30:02 +0700

    Update icons

--- src/components/Icons/StarIcon.tsx
+++ src/components/Icons/StarIcon.tsx
@@ -4,23 +4,22 @@ export const StarIcon = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
+      width={12}
+      height={12}
       fill="none"
       {...props}
     >
-      <g clip-path="url(#clip0_17_28197)">
+      <g clipPath="url(#a)">
         <path
-          d="M6 1L7.545 4.13L11 4.635L8.5 7.07L9.09 10.51L6 8.885L2.91 10.51L3.5 7.07L1 4.635L4.455 4.13L6 1Z"
           stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m6 1 1.545 3.13L11 4.635 8.5 7.07l.59 3.44L6 8.885 2.91 10.51l.59-3.44L1 4.635l3.455-.505L6 1Z"
         />
       </g>
       <defs>
-        <clipPath id="clip0_17_28197">
-          <rect width="12" height="12" fill="white" />
+        <clipPath id="a">
+          <path fill="#fff" d="M0 0h12v12H0z" />
         </clipPath>
       </defs>
     </svg>

efa89a9b7c79189ff9e7b245e410f79654e2c803
trungh97 <hoangquoctrungit@gmail.com>
2025-07-29 19:43:16 +0700

    Task #8: Jest installation and configuration

--- jest.config.ts
+++ jest.config.ts
@@ -90,7 +90,11 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
